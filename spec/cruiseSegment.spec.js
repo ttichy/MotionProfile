fc5e3a05ac867919981db35b30e381b2e80cae29
Tom Tichy <ttichy@yahoo.com>
2017-04-03 21:35:03 -0400

    clean up cruise/dwell segment code, write some unit tests

--- spec/cruiseSegment.spec.js
+++ spec/cruiseSegment.spec.js
@@ -5,10 +5,46 @@ describe('Unit: cruise/dwell segment testing', function() {
 
 
 
-    it('should create an cruise dwell segment with via distance (t0=0,tf=2,p0=0,v0=50) and correctly evalute position and velocities', function() {
+    it('should create an cruise dwell segment with via distance (t0=0,tf=2,p0=0,v0=50,pf=25) and correctly evalute position and velocities', function() {
 
-        var seg = cruiseSegmentFactory.makeWithDistance(0,0.5,0,50,25,'incremental');
+        var seg = cruiseSegmentFactory.make(0,0.5,0,50,25,'time','incremental');
+
+        expect(seg.evaluatePositionAt(0.25)).toBe(12.5);
+        expect(seg.evaluateVelocityAt(0.25)).toBe(50);
+        expect(seg.evaluatePositionAt(0.50)).toBe(25);
+
     });
+
+    it('should create an cruise dwell segment with via distance (t0=0,tf=2,p0=0,v0=0,pf=25) and correctly evalute position and velocities', function() {
+
+        var seg = cruiseSegmentFactory.make(0,0.5,0,0,0,'time','incremental');
+
+        expect(seg.evaluatePositionAt(0.25)).toBe(0);
+        expect(seg.evaluateVelocityAt(0.25)).toBe(0);
+        expect(seg.evaluatePositionAt(0.50)).toBe(0);
+
+    });    
+
+
+    it('should create a cruise segment via zero distance and starting with v0=0 (t0=0,tf=2,p0=0,v0=0,pf=0', function() {
+
+        var seg = cruiseSegmentFactory.make(0,0.5,0,0,0,'distance','incremental');
+
+        expect(seg.evaluatePositionAt(0.25)).toBe(0);
+        expect(seg.evaluateVelocityAt(0.25)).toBe(0);
+        expect(seg.evaluatePositionAt(0.50)).toBe(0);
+
+    });  
+
+
+    it('should throw when creating a cruise segment via non-zero distance and starting with v0=0 (t0=0,tf=0.5,p0=0,v0=0,pf=5', function() {
+
+    expect(function() {cruiseSegmentFactory.make(0,0.5,0,0,5,'distance','incremental');})
+        .toThrow(new Error("Unable to create a cruise/dwell segment with zero initial velocity and non zero distance"));
+
+
+
+    });    
 
 
 });
